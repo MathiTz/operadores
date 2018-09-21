8eff92b5bd5737b8b6fd9c2fc4118d5b827230d7
MathiTz <matheusalves789@gmail.com>
2018-09-21 11:50:27 -0300

    Operadores aritméticos

---
+++ exemplo-03.php
@@ -0,0 +1,30 @@
+<?php
+//                      Operadores Aritméticos
+
+$a = 10;
+$b = 2;
+// soma
+echo $a + $b;
+// subtração
+echo "<br >";
+echo $a - $b;
+// multiplicação
+echo "<br >";
+echo $a * $b;
+// divisão
+echo "<br >";
+echo $a / $b;
+// módulo (resto)
+echo "<br >";
+echo $a % $b;
+// exponenciação
+echo "<br >";
+echo $a ** $b;
+
+
+
+
+
+
+
+?>
