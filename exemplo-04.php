f2113388a6d7ae076f4b097ba1703d03c5c846b7
MathiTz <matheusalves789@gmail.com>
2018-09-21 11:56:34 -0300

    Operadores de comparação

---
+++ exemplo-04.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 30;
+
+$b = 55.0;
+// maior que
+var_dump($a > $b);
+echo "<br >";
+
+// menor que
+var_dump($a < $b);
+echo "<br >";
+
+// atribui o valor
+var_dump($a = $b);
+echo "<br >";
+
+// comparação de valor 
+var_dump($a == $b);
+echo "<br >";
+
+// comparação de tipo e valor (se são idênticos)
+var_dump($a === $b);
+echo "<br >";
+
+// comparação de diferença
+var_dump($a != $b);
+echo "<br >";
